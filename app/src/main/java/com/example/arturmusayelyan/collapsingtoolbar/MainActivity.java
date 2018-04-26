16eb6338c5a3b22217b9267a04b7293053220dba
artur.musayelyan <musayelyan099@gmail.com>
2018-04-26 18:59:29 +0400

    Initial commit

---
+++ app/src/main/java/com/example/arturmusayelyan/collapsingtoolbar/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.arturmusayelyan.collapsingtoolbar;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+    private FloatingActionButton fab;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar=findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        if(getSupportActionBar()!=null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+
+        fab=findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,SecondActivity.class));
+            }
+        });
+    }
+}
