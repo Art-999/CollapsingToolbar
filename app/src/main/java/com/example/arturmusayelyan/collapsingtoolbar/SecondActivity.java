8bd6c34592f7f6c167c7a9dffcd2b1071382794e
artur.musayelyan <musayelyan099@gmail.com>
2018-04-27 19:05:56 +0400

    27.04.2018 ArtCommit (1)

--- app/src/main/java/com/example/arturmusayelyan/collapsingtoolbar/SecondActivity.java
+++ app/src/main/java/com/example/arturmusayelyan/collapsingtoolbar/SecondActivity.java
@@ -1,13 +1,35 @@
 package com.example.arturmusayelyan.collapsingtoolbar;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.view.View;
 
-public class SecondActivity extends AppCompatActivity {
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+    private CardView cardViewBanking, cardViewIdeas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+        cardViewBanking = findViewById(R.id.banking_card);
+        cardViewIdeas = findViewById(R.id.ideas_card);
+        cardViewBanking.setOnClickListener(this);
+        cardViewIdeas.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.banking_card:
+                // Toast.makeText(this,"Banking card",Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(this, BankingActivity.class));
+                break;
+            case R.id.ideas_card:
+                startActivity(new Intent(this, ShopingActivity.class));
+                break;
+        }
     }
 }
