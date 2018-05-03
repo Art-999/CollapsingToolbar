49e72c62397f5390e5e06b50f27c115d00969c21
artur.musayelyan <musayelyan099@gmail.com>
2018-05-03 17:27:25 +0400

    03.05.2018 ArtCommit (2)

---
+++ app/src/main/java/com/example/arturmusayelyan/collapsingtoolbar/SplashScreenActivity.java
@@ -0,0 +1,43 @@
+package com.example.arturmusayelyan.collapsingtoolbar;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    private ImageView splashImageView;
+    private TextView splashTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        splashImageView = findViewById(R.id.splash_image_view);
+        splashTextView = findViewById(R.id.splash_text_view);
+        Animation myanim = AnimationUtils.loadAnimation(this, R.anim.mytransition);
+        splashTextView.startAnimation(myanim);
+        splashImageView.startAnimation(myanim);
+
+        final Intent goToMainActivityIntent = new Intent(this, MainActivity.class);
+        Thread timer = new Thread() {
+            public void run() {
+                try {
+                    sleep(5000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    startActivity(goToMainActivityIntent);
+                    //overridePendingTransition(R.anim.mytransition,R.anim.mytransition);
+                    finish();
+                }
+            }
+        };
+        timer.start();
+    }
+
+
+}
